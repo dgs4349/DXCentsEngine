d699a85ccdf5802a4e97b3c2b6899ef083058fd0
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2019-02-18 18:30:12 -0500

    Added Lua and Lua scriping libraries

--- EngineTest/main.cpp
+++ EngineTest/main.cpp
@@ -1,17 +1,33 @@
-#include "CentsEngine.h"
 
 #include <iostream>
 
-int main(void)
+#include "CentsEngine.h"
+
+int main()
 {
-	ObjectManager::GetInstance();
-	
-	GameObject* obj = new GameObject("Test Object");
-
-	LOG_TRACE("{}, {}", obj->ToString(), obj->transform->ToString());
 
 	getchar();
-	ObjectManager::ReleaseInstance();
-	getchar();
-	return 0;
+	return 1;
 }
+
+
+/*
+	extern "C" {
+#include "lua\lua.h"
+#include "lua\lauxlib.h"
+#include "lua\lualib.h"
+}
+
+#include <LuaBridge\LuaBridge.h>
+
+	using namespace luabridge;
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	getGlobalNamespace(L).addFunction("printMessage", printMessage);
+	luaL_dofile(L, "script.lua");
+	lua_pcall(L, 0, 0, 0);
+	LuaRef sumNumbers = getGlobal(L, "sumNumbers");
+	int result = sumNumbers(5, 4);
+	std::cout << "Result:" << result << std::endl;
+	system("pause");
+	*/
